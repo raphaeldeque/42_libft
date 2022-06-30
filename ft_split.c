bd7fe667fc521e332e496c961d90d7388af7962c
Raphael Rocha <rmoura-r@student.42.rio>
2022-06-30 20:00:53 -0300

    split no leaks

--- ft_split.c
+++ ft_split.c
@@ -6,29 +6,13 @@
 /*   By: rmoura-r <rmoura-r@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 17:40:38 by rmoura-r          #+#    #+#             */
-/*   Updated: 2022/06/30 16:22:53 by rmoura-r         ###   ########.fr       */
+/*   Updated: 2022/06/30 19:53:29 by rmoura-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_n_cell(const char *str, char c)
-{
-	int	i;
-	int	cell;
-
-	cell = 0;
-	i = -1;
-	while (str[++i] != '\0')
-	{
-		if (str[i + 1] == c || str[i + 1] == '\0')
-			if (str[i] != c)
-				cell++;
-	}
-	return (cell);
-}
-
-static int	ft_cellcpy(char **res, const char *str, char c)
+static int	ft_cellcpy(char **res, const char *s, char c)
 {
 	int	cell;
 	int	len;
@@ -36,23 +20,23 @@ static int	ft_cellcpy(char **res, const char *str, char c)
 
 	cell = 0;
 	len = 0;
-	i = 0;
-	while (str[i] != '\0')
+	i = -1;
+	while (s[++i] != '\0')
 	{
-		if (str[i] != c)
+		if (s[i] == c)
+			continue ;
+		len = 0;
+		while (s[i + len] != '\0' && s[i + len] != c)
+			len++;
+		res[cell] = malloc(sizeof(char) * (len + 1));
+		if (!res[cell])
 		{
-			len = 0;
-			while (str[i + len] != '\0' && str[i + len] != c)
-				len++;
-			res[cell] = malloc(sizeof(char) * (len + 1));
-			if (!res[cell])
-				return (0);
-			ft_strlcpy(res[cell], &str[i], (len + 1));
-			cell++;
-			i = i + len;
+			while (cell >= 0)
+				free(res[cell--]);
+			return (0);
 		}
-		else
-			i++;
+		ft_strlcpy(res[cell++], &s[i], (len + 1));
+		i = i + len - 1;
 	}
 	return (1);
 }
@@ -61,10 +45,15 @@ char	**ft_split(char const *s, char c)
 {
 	char	**res;
 	int		cells;
+	int		i;
 
 	if (!s)
 		return (NULL);
-	cells = ft_n_cell(s, c);
+	i = -1;
+	cells = 0;
+	while (s[++i] != '\0')
+		if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+				cells++;
 	res = malloc(sizeof(char *) * (cells + 1));
 	if (!res)
 		return (NULL);
